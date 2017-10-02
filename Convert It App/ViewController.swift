8376e8f8f9ef7cf97dcdc9500113e739f3cfb697
Michelle Krameisen <michellekrameisen@michelles-macbook-pro-2.local>
2017-10-02 15:44:29 -0400

    midway through guard

--- Convert It App/ViewController.swift
+++ Convert It App/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var fromUnitsLabel: UILabel!
     @IBOutlet weak var resultsLabel: UILabel!
     @IBOutlet weak var formulaPicker: UIPickerView!
+    @IBOutlet weak var decimalSegment: UISegmentedControl!
     
     var formulasArray = ["miles to kilometers",
                          "kilometers to miles",
@@ -34,36 +35,40 @@ class ViewController: UIViewController {
     
     func calculateConversion() {
         
-        var outputValue = 0.0
-        
-        if let inputValue = Double(userInput.text!) {
-            
-            switch conversionStrings {
-            case "miles to kilometers":
-            outputValue = inputValue / 0.62137
-            case "kilometers to miles":
-            outputValue = inputValue * 0.62137
-            case "feet to meters":
-            outputValue = inputValue / 3.2808
-            case "yards to meters":
-            outputValue = inputValue / 1.0936
-            case "meters to feet":
-            outputValue = inputValue * 3.2808
-            case "meters to yards":
-            outputValue = inputValue * 1.0936
-            default:
-                print("show alert - for some reason we didn't have a ConverstionStrings")
-            }
-            resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
-            
-        } else {
+        guard let inputValue = Double(userInput.text!) else {
             print("show alert here to say the value entered was not a number")
+            return
         }
-    }
-    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        var outputValue = 0.0
+        switch conversionStrings {
+        case "miles to kilometers":
+            outputValue = inputValue / 0.62137
+        case "kilometers to miles":
+            outputValue = inputValue * 0.62137
+        case "feet to meters":
+            outputValue = inputValue / 3.2808
+        case "yards to meters":
+            outputValue = inputValue / 1.0936
+        case "meters to feet":
+            outputValue = inputValue * 3.2808
+        case "meters to yards":
+            outputValue = inputValue * 1.0936
+        default:
+            print("show alert - for some reason we didn't have a ConverstionStrings")
+        }
+        resultsLabel.text = "\(inputValue) \(fromUnits) = \(outputValue) \(toUnits)"
     }
 
+    
+    @IBAction func convertButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func decimalSelected(_ sender: UISegmentedControl) {
+        
+    }
 }
+
+
 
 extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
